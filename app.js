de8256d8a515215ff50e9f2e0ddcee03c73ea9ce
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-12-24 11:11:42 -0500

    create all the important contrutor functions

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // global variables
 
+/**
+ * extract dinos data from the dino.json file
+ */
 function getData() {
   return fetch('./dino.json')
     .then((res) => res.json())
@@ -7,21 +10,47 @@ function getData() {
     .catch((err) => console.log(err));
 }
 
-// Create Dino Constructor
-function Dino(species, weight, height, diet, origin, when, fact) {
-  this.species = species;
+// Super class. Dino and Human classes inherit from it
+function Creature(weight, height, diet) {
   this.weight = weight;
   this.height = height;
   this.diet = diet;
+}
+
+// Human constructor function. It inherits from the Creature
+function Human(name, weight, height, diet) {
+  Creature.apply(this, [weight, height, diet]);
+  this.name = name;
+}
+
+// Dino Constructor function. It inherits from the Creature
+function Dino(species, weight, height, diet, origin, when, fact) {
+  Creature.apply(this, [weight, height, diet]);
+  this.species = species;
   this.origin = origin;
   this.when = when;
   this.fact = fact;
 }
 
+// This IIFE initialize the app by setting the prorotypal inheritance
+(function initializeApp() {
+  Human.prototype = Object.create(Creature.prototype);
+  Dino.prototype = Object.create(Creature.prototype);
+  Creature.prototype.compareHeight = function (obj) {
+    console.log(this.height);
+  };
+  Creature.prototype.compareWeight = function () {
+    return this.weight;
+  };
+  Creature.prototype.compareDiet = function (obj) {
+    console.log(this.diet);
+  };
+})();
+
 // Create Dino Objects
 async function createDinoObject() {
   const dinos = await getData();
-  dinosObjects = dinos['Dinos'].map(
+  const dinosObjects = dinos['Dinos'].map(
     (dino) =>
       new Dino(
         dino.species,
@@ -33,8 +62,9 @@ async function createDinoObject() {
         dino.fact
       )
   );
-
   console.log(dinosObjects);
+  console.log(dinosObjects[0].compareWeight());
+  return dinosObjects;
 }
 
 createDinoObject();
@@ -63,7 +93,12 @@ document.getElementById('btn').addEventListener(
   'click',
   (function () {
     const form = document.getElementById('dino-compare');
-    // IIFE returning a function which is closing over the form variable
+    const nameInput = document.getElementById('name');
+    const feetInput = document.getElementById('feet');
+    const inchesInput = document.getElementById('inches');
+    const weightInput = document.getElementById('weight');
+    const dietSelect = document.getElementById('diet');
+    // IIFE returning a function which is closing over the form variable.
     // the returned can access the IIFE's score because of closure even
     // after being returned.
     return function () {
