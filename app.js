70398af72cf37f456a1f008bc8c8ce765fd9af28
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-12-23 21:35:55 -0500

    remove form once compare button is clicked

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
+// global variables
 
-    // Create Dino Constructor
+// Create Dino Constructor
 
+// Create Dino Objects
 
-    // Create Dino Objects
+// Create Human Object
 
+// Use IIFE to get human data from form
 
-    // Create Human Object
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Use IIFE to get human data from form
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+// Generate Tiles for each Dino in Array
 
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Add tiles to DOM
 
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
-
-    // Remove form from screen
-
+// Remove form from screen
 
 // On button click, prepare and display infographic
+document.getElementById('btn').addEventListener(
+  'click',
+  (function () {
+    const form = document.getElementById('dino-compare');
+    // IIFE returning a function which is closing over the form variable
+    // the returned can access the IIFE's score because of closure even
+    // after being returned.
+    return function () {
+      form.style.display = 'none';
+    };
+  })()
+);
